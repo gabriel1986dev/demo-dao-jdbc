e36d3ab426a7b7fd05adbcd0cbe876ea8efce03c
gabriel1986dev <etevaldo1986@yahoo.com>
2024-05-31 13:00:16 -0300

    FindByld implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,10 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department object = new Department(1, "book");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, object);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        
+
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }
